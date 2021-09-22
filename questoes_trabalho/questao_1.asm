22ea680309a8f4ea0430d3617617959dd148f583
jpdrslvr <jpdrslvr@gmail.com>
2021-09-22 13:51:20 -0300

    questão 1 refeita

--- questoes_trabalho/questao_1.asm
+++ questoes_trabalho/questao_1.asm
@@ -5,43 +5,49 @@
 #   if(i%2==0)
 #     vetor[i] = i * 2;
 #   else
-#     vetor[i] = i + vetor[i-1];
+#     vetor[i] = vetor[i] + vetor[i-1];
 # }
 # Cada posição do vetor deve ter 4 bytes e deve ser armazenada, em sequência, a
 # partir da posição 0x1001000 da memória.
 
 .data
-	vetor: .space 24
+	vetor: .space 24 # declara vetor de 8 posições
 	
 .text
 	lui $t0, 0x1001 # início da memória de dados
-	ori $t5, $zero, 7 # tamanho do vetor
+	ori $t6, $zero, 7 # tamanho do vetor
 	ori $t1, $zero, 2 # pra comparar se par/ímpar
 	
 	loop:
 		
+		lw $t4, 0x0($t0) # lê valor na posiçao i
+	
 		# testa se $t2 é par
 		div $t2, $t1
 		mfhi $t3
 		beq $t3, $zero, prod
 		
 		# soma
-		add $t4, $t2, $t4
+		add $t5, $t5, $t4 # vetor[i] + vetor[i-1]
 		j store
 		
 		# produto
 		prod:
-			sll $t4, $t2, 1 # i * 2
+			sll $t5, $t2, 1 # i * 2
 					
 		store:
+		
+		# copia anterior (vetor[i-1]) para t4
+		or $t4, $t5, $zero
+		
 		# armazena resultado na memória
-		sw $t4, 0x0($t0)
+		sw $t5, 0x0($t0)
 		
 		# incrmenta registrador base para a próxima posição da memória
 		addi $t0, $t0, 4
 		
 		# fim do loop
-		beq $t2, $t5, exit
+		beq $t2, $t6, exit
 		
 		# incrementa contador
 		addi $t2, $t2, 1
