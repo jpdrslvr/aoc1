4a3a41d9d124830cd1dc87c3ab420805602bfe10
jpdrslvr <jpdrslvr@gmail.com>
2021-09-23 22:25:13 -0300

    questão 4 ajuste

--- questoes_trabalho/questao_4.asm
+++ questoes_trabalho/questao_4.asm
@@ -3,19 +3,21 @@
 # ser escrito na posição seguinte na memória (0x10010004).
 
 .data
-	n: .word 8
+	n: .word 10
 	
 .text
-	lui $t0,  0x1001
+	lui $t0,  0x1001 # posição inicial da memória
 	
 	lw $t1, 0x0($t0) # $t1 = n
 	
 	ori $t2, $zero, 1 # valor inicial de n!
-	
-	loop:
-		beq $t1, $zero, exit # n == 0
 		
-		mult $t2, $t1 # n * (n - 1)
+	loop:
+		
+		slti $t3, $t1, 2 # $t3 = 1 se n < 2
+		bne $t3, $zero, exit # n < 2
+		
+		mult $t1, $t2 # n * (n - 1)
 		mflo $t2
 		
 		addiu $t1, $t1, -1 # n -= 1
